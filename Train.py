83c1a107e69b46a3a782cae36424c2f768958662
Aaron Jimenez <aaronjimv@gmail.com>
2023-08-22 21:01:28 -0800

    se tradujo los comentarios del archivo train.py al ingles

--- Train.py
+++ Train.py
@@ -1,8 +1,9 @@
 import openai
 
-# Configurar la clave de la API de OpenAI
+# Set up the OpenAI API key
 openai.api_key = "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"
 
+# Import libraries
 from llama_index import SimpleDirectoryReader, GPTListIndex, GPTVectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext
 from llama_index import StorageContext, load_index_from_storage
 import os
@@ -10,32 +11,34 @@ import gradio as gr
 from langchain.embeddings import OpenAIEmbeddings
 from langchain import OpenAI
 
-# Configurar la clave de la API de OpenAI en la variable de entorno
+# Set up the OpenAI API key as an environment variable
 os.environ['OPENAI_API_KEY'] = openai.api_key
 
-# Cargar datos de entrenamiento desde un directorio llamado "datos"
+# Load training data from a directory named "datos"
 docs = SimpleDirectoryReader("datos").load_data()
 
+# Define parameters
 max_input = 4098
 tokens = 256
 chnk_size = 600
 max_chnk_overlap = 0.2
 
+# Define the training function
 def entrenamiento(path):
-    # Crear un PromptHelper para procesar las solicitudes
+    # Create a PromptHelper to process requests
     Prompt_helper = PromptHelper(max_input, tokens, max_chnk_overlap, chunk_size_limit=chnk_size)
     
-    # Crear un modelo de LLMPredictor con un modelo de OpenAI preentrenado
+    # Create an LLMPredictor model with a pre-trained OpenAI model
     modelo = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-ada-001", max_tokens=tokens))
     
-    # Crear un contexto de servicio utilizando el modelo y el PromptHelper
+    # Create a service context using the model and the PromptHelper
     contexto = ServiceContext.from_defaults(llm_predictor=modelo, prompt_helper=Prompt_helper)
         
-    # Entrenar un índice GPTVectorStoreIndex utilizando los documentos y el contexto del servicio
+    # Train a GPTVectorStoreIndex using the documents and the service context
     index_model = GPTVectorStoreIndex.from_documents(docs,service_context=contexto)
     
-    # Guardar el índice entrenado en disco en un directorio llamado "Modelo"
+    # Save the trained index to disk in a directory named "Modelo"
     index_model.storage_context.persist(persist_dir= 'Modelo') 
 
-# Llamar a la función de entrenamiento con el directorio de datos "datos"
+# Call the training function with the "datos" directory
 entrenamiento("datos")
