80cb09f4df87090bce32435a0969c547d44c797e
Aaron Jimenez <aaronjimv@gmail.com>
2023-08-22 21:08:10 -0800

    se tradujo al ingles los comentarios de predic.py

--- Predict.py
+++ Predict.py
@@ -1,8 +1,9 @@
 import openai
 
-# Configurar la clave de la API de OpenAI
+# Set up the OpenAI API key
 openai.api_key = "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"
 
+# Import libraries
 from llama_index import SimpleDirectoryReader, GPTListIndex, GPTVectorStoreIndex, LLMPredictor, PromptHelper, ServiceContext
 from llama_index import StorageContext, load_index_from_storage
 import os
@@ -10,25 +11,29 @@ import gradio as gr
 from langchain.embeddings import OpenAIEmbeddings
 from langchain import OpenAI
 
-# Configurar la clave de la API de OpenAI en la variable de entorno
+# Set up the OpenAI API key as an environment variable
 os.environ['OPENAI_API_KEY'] = openai.api_key
 
+# Define the chatbot function
 def chatbot(question):
-    # Cargar el índice entrenado desde el directorio "Modelo"
+    # Load the trained index from the "Modelo" directory
     storage_context = StorageContext.from_defaults(persist_dir='Modelo')
     index = load_index_from_storage(storage_context)
+    
+    # Create a query engine from the loaded index
     query_engine = index.as_query_engine()
     
-    # Realizar una consulta al índice con la pregunta ingresada y obtener la respuesta generada
+    # Perform a query on the index with the entered question and get the generated response
     resonse = query_engine.query(question)
 
+    # Return the response
     return resonse
 
-# Crear una interfaz gráfica para el chatbot utilizando Gradio
+# Create a graphical interface for the chatbot using Gradio
 app = gr.Interface(fn=chatbot,
                    inputs= gr.inputs.Textbox(lines=5, label="Ingrese una peticion"),
                    outputs= "text",
                    title="Chatbot")
 
-# Lanzar la interfaz gráfica
+# Launch the graphical interface
 app.launch()
